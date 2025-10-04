7f8b0a876ecc1073ccdff37d9c8a4d8b29077e88
san2804 <kmsandildm@gmail.com>
2025-10-04 11:29:08 +0530

    Initial Commit

---
+++ FinGuard/FinGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinGuardApp.swift
+//  FinGuard
+//
+//  Created by Sandil on 2025-10-04.
+//
+
+import SwiftUI
+
+@main
+struct FinGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
